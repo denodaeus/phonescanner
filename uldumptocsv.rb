b23f9268674ac64e3fbd9b41c220e0b80aa2b292
bsmith <bsmith@0776f297-9ff4-4979-ac48-aec5c08da478>
2012-07-27 02:05:38 +0000

    PT-51: remove q values from contact if they exist, causing some parse issues when automating the usrloc scrape and we don't need them
    git-svn-id: http://69.61.97.221:81/svn/tools/trunk/infr/polyscan@67842 0776f297-9ff4-4979-ac48-aec5c08da478

--- uldumptocsv.rb
+++ uldumptocsv.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+# script to convert from opensips mi_fifo output to polyscan expected input
+# ./uldumptocsv.rb FILE_NAME
+
 dump_file=ARGV[0]
 
 File.open("proxy/#{dump_file}-parsed.csv", "w") { |out_file|
@@ -18,16 +21,13 @@ File.open("proxy/#{dump_file}-parsed.csv", "w") { |out_file|
             entry[0] = sipid + "." + sipid
         elsif line =~ /Contact/
             new_entry = false
-            entry[1] = line.split("Contact:: ")[1].chomp
+            entry[1] = line.split("Contact:: ")[1].split(" Q=")[0].chomp
         elsif line =~ /Received/
             new_entry = false
             entry[2] = line.split("Received:: ")[1].chomp
         elsif line =~ /User-agent/
             new_entry = false
             entry[3] = line.split("User-agent:: ")[1].chomp
-        #elsif line =~ /Callid/
-        #    new_entry = false
-        #    entry[4] = line.split("Callid:: ")[1].chomp
         end
     end
 }
