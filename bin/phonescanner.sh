6496ef3b63280670145559b7fe86b0ac64fe4470
bsmith <bsmith@0776f297-9ff4-4979-ac48-aec5c08da478>
2012-08-27 16:31:51 +0000

    PT-51: merge proxy files when there are multiple hosts to check, generate html report as well
    git-svn-id: http://69.61.97.221:81/svn/tools/trunk/infr/polyscan@69294 0776f297-9ff4-4979-ac48-aec5c08da478

--- bin/phonescanner.sh
+++ bin/phonescanner.sh
@@ -30,9 +30,18 @@ process_ul() {
     done
 }
 
+merge_files() {
+	$PSPATH/proxymerge.rb
+}
+
+generate_html() {
+	$PSPATH/makehtml.rb
+}
+
 prepare_results() {
     sort -u -t, -k6,6 $FILE | sort -t, -k1,1 > $FILE-$DATE-unique.csv
     cp $PSPATH/output/$FILE-$DATE-unique.csv $PSPATH/output/polyscan-results.csv
+    generate_html
     tar cvzf $PSPATH/output/results-$DATE.tar.gz $PSPATH/output/polyscan*
 }
 
@@ -43,6 +52,7 @@ publish_results() {
 main() {
     fetch_userdump
     process_ul
+    merge_files
     execute_script
     prepare_results
     publish_results
