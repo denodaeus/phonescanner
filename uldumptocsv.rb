10ddae1dd0decff0daec7ea284eee4a962a12464
bsmith <bsmith@0776f297-9ff4-4979-ac48-aec5c08da478>
2012-11-09 19:27:38 +0000

    PT-171: clean up directory
    git-svn-id: http://69.61.97.221:81/svn/tools/trunk/infr/polyscan@72255 0776f297-9ff4-4979-ac48-aec5c08da478

--- uldumptocsv.rb
+++ uldumptocsv.rb
@@ -5,7 +5,7 @@
 
 dump_file=ARGV[0]
 
-File.open("proxy/#{dump_file}-parsed.csv", "w") { |out_file|
+File.open("#{dump_file}-parsed.csv", "w") { |out_file|
     line = []
     new_entry = false
     entry = []
